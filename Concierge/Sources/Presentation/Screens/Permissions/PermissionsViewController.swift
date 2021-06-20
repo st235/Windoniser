01142175d7a8fc38676a05b9e1e38dc1c4b72063
Alexander Dadukin <hi@st235.xyz>
2021-06-20 13:56:40 +0100

    permission prompt design fixes.

--- Concierge/Sources/Presentation/Screens/Permissions/PermissionsViewController.swift
+++ Concierge/Sources/Presentation/Screens/Permissions/PermissionsViewController.swift
@@ -3,16 +3,31 @@ import Foundation
 class PermissionsViewController: NSViewController {
     
     private var accessibilityPermissionsManager: AccessibilityPermissionsManager = AppDependenciesResolver.shared.resolve(type: AccessibilityPermissionsManager.self)
+    
     @IBOutlet weak var grantPermissionButton: NSButton!
+    @IBOutlet weak var contentDescriptionLabel: NSTextField!
+    @IBOutlet weak var closeApplicationButton: NSButton!
     
     override func viewDidLoad() {
+        contentDescriptionLabel.stringValue = "permissions_screen_description".localized
+        grantPermissionButton.title = "permissions_grant_button_text".localized
+        closeApplicationButton.title = "permissions_close_app_button_text".localized
+        
         grantPermissionButton.target = self
         grantPermissionButton.sendAction(on: .leftMouseUp)
         grantPermissionButton.action = #selector(onButtonClicked(_:))
+        
+        closeApplicationButton.target = self
+        closeApplicationButton.sendAction(on: .leftMouseUp)
+        closeApplicationButton.action = #selector(onQuitApplicationClicked(_:))
     }
     
     @objc private func onButtonClicked(_ sender: Any?) {
         accessibilityPermissionsManager.requestPermission()
+    }
+    
+    @objc private func onQuitApplicationClicked(_ sender: Any?) {
+        NSApp.terminate(nil)
     }
     
 }
